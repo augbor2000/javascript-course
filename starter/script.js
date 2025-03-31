45fd86d99e36dc2d162836baffde13f476d610e3
augbor2000 <augustinborozan2000@gmail.com>
2025-03-31 20:26:31 +0300

    My first JavaScript code

---
+++ starter/script.js
@@ -0,0 +1,98 @@
+/*let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function =27;
+
+let person ='jonas';
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof (true));
+console.log(typeof (javascriptIsFun));
+//console.log(typeof (23));
+//console.log(typeof ('Jonas'));
+
+javascriptIsFun = "YES!";
+console.log(typeof (javascriptIsFun));
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+
+//LET,CONST & VAR
+//----------------
+
+//REASSIGN 
+let age = 31;
+age = 31;
+
+// Value does not change
+const birthYear = 1991;
+//birthYear=1990; //ERROR
+
+//const =job; // ERROR we need to initialize a value to job
+
+// Similar with let 
+// Old way
+var job = "programmer";
+job = "teacher";
+
+
+//BASIC OPERATORS
+//---------------
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(ageJonas, ageSarah);
+
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3); // 2 ** 3 -> 2to the power of 3->2*2*2
+
+const firstName = "Jonas";
+const lastName = "Schmedtman";
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+let x = 10 + 5; //15
+x += 10; // x = x + 10 =25 Reassign
+x *= 4; // x = x * 4 = 100
+x++; //x = x+1
+x--; // x = x - 1
+console.log(x);
+
+// Comparison operrators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+*/
